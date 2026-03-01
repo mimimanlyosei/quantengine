a28444b844ae7667621a9f9b462928e3922add7f
Mimi Manly-Osei <missmosei1@gmail.com>
2026-03-01 20:01:04 +0000

    Added the 4 routes I believe I will need for the app.

--- app.py
+++ app.py
@@ -7,5 +7,26 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+@app.route("/register")
+def register():
+    return "register"
+
+
+@app.route("/login")
+def login():
+    return "login"
+
+
+@app.route('/calculate')
+def calculate():
+    return "calculate"
+
+
+@app.route("/history")
+def history():
+    return "history"
+
+
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=5555, debug=True)
