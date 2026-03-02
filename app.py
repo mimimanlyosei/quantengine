24d51c3ad36230d6e5b5e6f0e64a6c198f918941
Mimi Manly-Osei <missmosei1@gmail.com>
2026-03-02 02:34:47 +0000

    added app config for db. added test for db. created db file

--- app.py
+++ app.py
@@ -1,32 +1,70 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from sqlalchemy import text
 
-app = Flask(__name__)
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app():
+    '''
+    This function creates an instance of my app.
+    This will later allow me to duplicate as as when needed and test/reconfigure independently.
+    '''
+
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'this-is-a-secret'
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///app.db"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+    @app.route("/health/db")
+    def health_db():
+        try:
+            db.session.execute(text("SELECT 1"))
+            return {"db": "ok"}, 200
+        except Exception as e:
+            return{"db": "error", "detail": str(e)}, 500
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+    @app.route("/")
+    def index():
+        return render_template("index.html")
 
 
-@app.route("/register")
-def register():
-    return render_template("register.html")
+    @app.route("/register")
+    def register():
+        return render_template("register.html")
 
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
+    @app.route("/login")
+    def login():
+        return render_template("login.html")
+    
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return None
 
 
-@app.route('/calculate')
-def calculate():
-    return render_template("calculate.html")
+    return app
 
 
-@app.route("/history")
-def history():
-    return render_template("history.html")
+
+# @app.route('/calculate')
+# def calculate():
+#     return render_template("calculate.html")
+
+
+# @app.route("/history")
+# def history():
+#     return render_template("history.html")
 
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(host="127.0.0.1", port=5555, debug=True)
