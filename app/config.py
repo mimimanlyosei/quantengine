7d53af9c967f58b9154e6d4eaa9b253bf055c5a8
Mimi Manly-Osei <mimimanlyosei@gmail.com>
2026-04-20 13:07:39 +0100

    Deployment: Created the Procfile and render.yaml files and edited .gitignore and rewuirements.txt. App is still working as it should. relates to #35

--- app/config.py
+++ app/config.py
@@ -1,6 +1,11 @@
+import os
+
 class Config:
     """Configuration settings for quantEngine"""
-    SECRET_KEY = 'this-is-a-secret'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SECRET_KEY = os.getenv('SECRET_KEY') or 'this-is-a-secret'
+    _db_url = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
+    if _db_url and _db_url.startswith('postgres://'):
+        _db_url = _db_url.replace('postgres://', 'postgresql://', 1)
+    SQLALCHEMY_DATABASE_URI = _db_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
